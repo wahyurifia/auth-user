c983b2bccd54b2aef141e154eeb49282e7cce11c
Wahyu Rifia Rizki <wahyurifia927@gmail.com>
2024-11-13 20:24:54 +0700

    tambahin

--- server/src/middleware/auth.ts
+++ server/src/middleware/auth.ts
@@ -20,7 +20,7 @@ export const authMiddleware = (
     next();
   } catch (error) {
     res.redirect("/api/auth/login");
-    res.status(401).json({ message: "Invalid token" });
+    // res.status(401).json({ message: "Invalid token" });
   }
 };
 
