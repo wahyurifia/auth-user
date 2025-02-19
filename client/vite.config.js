6b3243a0a90c1adf2baa054177c78205009af7cd
Wahyu Rifia Rizki <wahyurifia927@gmail.com>
2025-02-19 20:15:25 +0700

    refresh deploy

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,9 +1,18 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import path from "path";
 
 export default defineConfig({
   plugins: [react()],
   resolve: {
-    alias: [{ find: "@", replacement: "/src" }],
+    alias: {
+      "@": path.resolve(__dirname, "src"), // Alias untuk folder src
+    },
+  },
+  server: {
+    historyApiFallback: true, // Fix error 404 saat refresh di route selain "/"
+  },
+  preview: {
+    historyApiFallback: true,
   },
 });
